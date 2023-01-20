ff721f357082568ff23e1810a035263e1e548d58
YulyBulatov <yulik.bulatov@gmail.com>
2023-01-20 08:56:50 +0100

    working on front

--- view/home.php
+++ view/home.php
@@ -1,9 +1,11 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<div class="site-description">
+    <h1 class="title-huge">BIENVENUE SUR LE FORUM</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+    <p class="desc-long">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
-<p>
-    <a href="index.php?ctrl=security&action=login">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="index.php?ctrl=security&action=register">S'inscrire</a>
-</p>
+    <p>
+        <a href="index.php?ctrl=security&action=login">Se connecter</a>
+        <span>&nbsp;-&nbsp;</span>
+        <a href="index.php?ctrl=security&action=register">S'inscrire</a>
+    </p>
+</div>
--- view/layout.php
+++ view/layout.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.tiny.cloud/1/zg3mwraazn1b2ezih16je1tc6z7gwp5yd4pod06ae5uai8pa/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <link rel="stylesheet" href="<?= PUBLIC_DIR ?>/css/style.css">
+    <link rel="stylesheet" href="public/css/style.css">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <title>FORUM</title>
 </head>
 <body>
@@ -17,43 +18,41 @@
             <h3 class="message" style="color: red"><?= App\Session::getFlash("error") ?></h3>
             <h3 class="message" style="color: green"><?= App\Session::getFlash("success") ?></h3>
             <header>
-                <nav>
-                    <div id="nav-left">
-                        <a href="index.php?ctrl=home&action=index">Accueil</a>
-                        <?php
-                        if(App\Session::isAdmin()){
-                            ?>
-                            <a href="index.php?ctrl=security&action=users">Voir la liste des gens</a>
-                          
-                            <?php
-                        }
-                        ?>
+                    <div class = header-overlay>
+                        <nav>
+                            <ul>
+                                <li><a href="index.php?ctrl=home&action=index"><span>Accueil</span></a></li>
+                                <?php
+                                if(App\Session::isAdmin()){
+                                    ?>
+                                    <li><a href="index.php?ctrl=security&action=users"><span>Voir la liste des gens</span></a></li>
+                                
+                                    <?php
+                                }
+                                ?>                    
+                                <?php
+                                
+                                if(App\Session::getUser()){
+                                    ?>
+                                    <li><a href="index.php?ctrl=security&action=viewProfile&id=<?=$_SESSION['user']->getId()?>"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a></li>
+                                    <li><a href="index.php?ctrl=security&action=logout"><span>Déconnexion</span></a></li>
+                                    <li><a href="index.php?ctrl=forum&action=listCategories"><span>La liste des categories</span></a></li>
+                                    <?php
+                                }
+                                else{
+                                    ?>
+                                    <li><a href="index.php?ctrl=security&action=login"><span>Connexion</span></a></li>
+                                    <li><a href="index.php?ctrl=security&action=register"><span>Inscription</span></a></li>
+                                    <li><a href="index.php?ctrl=forum&action=listCategories"><span> liste des categories</span></a></li>
+                                <?php
+                                }           
+                                ?>
+                            </ul>
+                        </nav>
                     </div>
-                    <div id="nav-right">
-                    <?php
-                        
-                        if(App\Session::getUser()){
-                            ?>
-                            <a href="index.php?ctrl=security&action=viewProfile&id=<?=$_SESSION['user']->getId()?>"><span class="fas fa-user"></span>&nbsp;<?= App\Session::getUser()?></a>
-                            <a href="index.php?ctrl=security&action=logout">Déconnexion</a>
-                            <a href="index.php?ctrl=forum&action=listCategories">La liste des categories</a>
-                            <?php
-                        }
-                        else{
-                            ?>
-                            <a href="index.php?ctrl=security&action=login">Connexion</a>
-                            <a href="index.php?ctrl=security&action=register">Inscription</a>
-                            <a href="index.php?ctrl=forum&action=listCategories">La liste des categories</a>
-                        <?php
-                        }
-                   
-                        
-                    ?>
-                    </div>
-                </nav>
             </header>
             
-            <main id="forum">
+            <main class="forum-main">
                 <?= $page ?>
             </main>
         </div>
